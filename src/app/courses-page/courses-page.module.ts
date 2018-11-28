cd7b3d8c90ec4c67a9891846dace3c49ad0b508b
Alexander Golub <golub.alexander.al@gmail.com>
2018-11-28 22:43:59 +0300

    add course page navigation components

--- src/app/courses-page/courses-page.module.ts
+++ src/app/courses-page/courses-page.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoursesPageComponent } from './courses-page/courses-page.component';
 import { CourseComponent } from './courses-page/course/course.component';
+import { NavigationComponent } from './courses-page/navigation/navigation.component';
+import { SearchComponent } from './courses-page/navigation/search/search.component';
+import { NewCourseComponent } from './courses-page/navigation/new-course/new-course.component';
 
 @NgModule({
-  declarations: [CoursesPageComponent, CourseComponent],
+  declarations: [CoursesPageComponent, CourseComponent, NavigationComponent, SearchComponent, NewCourseComponent],
   imports: [
     CommonModule
   ],
   exports: [
-    CourseComponent
+    CoursesPageComponent
   ]
 })
 export class CoursesPageModule { }
