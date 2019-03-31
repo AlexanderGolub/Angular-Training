ff975fa2d973c8ff5c0f1de17b63e9660e742a23
Alexander Golub <golub.alexander.al@gmail.com>
2019-03-31 17:33:06 +0300

    use reactive forms, add validation

--- src/app/courses-page/edit-course-page/edit-course-page.component.ts
+++ src/app/courses-page/edit-course-page/edit-course-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { CoursesService } from '../services/courses.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-course-page',
@@ -8,9 +9,11 @@ import { CoursesService } from '../services/courses.service';
   styleUrls: ['./edit-course-page.component.css']
 })
 export class EditCoursePageComponent implements OnInit {
-  title: string = '';
-  description: string = '';
-  duration: string = '';
+  courseForm = new FormGroup({
+    title: new FormControl('', [Validators.required, Validators.maxLength(50)]),
+    description: new FormControl('', [Validators.required, Validators.maxLength(500)]),
+    duration: new FormControl('', [Validators.required]),
+  });
   courseId: string = '';
 
   constructor(
@@ -20,28 +23,35 @@ export class EditCoursePageComponent implements OnInit {
   ) {
   }
 
+  get title() { return this.courseForm.get('title'); }
+  get description() { return this.courseForm.get('description'); }
+  get duration() { return this.courseForm.get('duration'); }
+
   ngOnInit() {
     this.route.params.subscribe((params) => {
       this.coursesService.getCourseById(params.id).subscribe((courses: any) => {
         const course = courses[0];
 
         this.courseId = course.id;
-        this.title = course.title;
-        this.description = course.description;
-        this.duration = course.duration.toString();
+
+        this.courseForm.setValue({
+          title: course.title,
+          description: course.description,
+          duration: course.duration.toString(),
+        });
       });
     });
   }
 
   onSave() {
-    this.coursesService.updateCourse({
-      id: this.courseId,
-      title: this.title,
-      description: this.description,
-      duration: this.duration
-    }).subscribe(() => {
-      this.router.navigate(['courses']);
-    });
+    // this.coursesService.updateCourse({
+    //   id: this.courseId,
+    //   title: this.title,
+    //   description: this.description,
+    //   duration: this.duration
+    // }).subscribe(() => {
+    //   this.router.navigate(['courses']);
+    // });
   }
 
   onCancel() {
